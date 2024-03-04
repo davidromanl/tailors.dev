31b6a59eb8555e19761072265f76fb027782d6a6
David Roman <roman.david@gmail.com>
2024-03-04 17:41:49 -0500

    add storyblok and vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,22 @@
 import { defineConfig } from 'astro/config';
-
+import storyblok from "@storyblok/astro";
+import { loadEnv } from 'vite';
 import tailwind from "@astrojs/tailwind";
+import vue from "@astrojs/vue";
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      page: 'storyblok/Page',
+      html: 'storyblok/Html'
+    },
+    enableFallbackComponent: true,
+    apiOptions: {
+      region: "us"
+    }
+  }), tailwind(), vue()]
 });
