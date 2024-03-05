111c8103d7ba8406ea06a5be7b7cf96a66fc97d2
David Roman <roman.david@gmail.com>
2024-03-05 02:57:57 -0500

    add html

---
+++ storyblok/Html.vue
@@ -0,0 +1,10 @@
+<template>
+    <div v-editable="blok" class="px-4">
+        <div v-html="content"></div>
+    </div>
+</template>
+
+<script setup>
+  const props = defineProps({ blok: Object })
+  const content = computed(() => renderRichText(props.blok.Content));
+</script>
