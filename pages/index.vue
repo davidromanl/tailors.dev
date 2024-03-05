4e6bb780a04e7ff157fdf09ef7b7aa6c4f6864ea
David Roman <roman.david@gmail.com>
2024-03-05 02:44:09 -0500

    add block

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
+<script setup>
+const story = await useAsyncStoryblok('home', { version: 'draft' })
+</script>
+
 <template>
-    <div>
-        Index
+    <div class="w-max-6xl mx-auto px-8">
+        <StoryblokComponent v-if="story" :blok="story.content" />
     </div>
 </template>
