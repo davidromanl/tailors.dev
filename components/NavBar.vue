417290884ec3a728a9cff0d7754a90aa9abae17d
Jhon1505e <lopezjhonedison20@gmail.com>
2025-07-31 19:42:59 -0500

    Diseño vista

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -3,12 +3,12 @@
     <div
       class=" fixed w-full z-20 backdrop-blur bg-black/10 py-4 px-10 flex justify-between font-cal"
     > 
-    <div class="flex justify-between w-full max-w-6xl mx-auto">
+    <div class="flex justify-center w-full max-w-6xl mx-auto">
 
         
-        <h2 class="text-white font-black text-4xl font-paris">
+        <!-- <h2 class="text-white font-black text-4xl font-paris">
         Tailors<span class="text-indigo-600">Dev</span>
-      </h2>
+      </h2> -->
       <div class="flex items-center gap-4">
         <NuxtLink
           to="/"
@@ -29,7 +29,7 @@
 
         <NuxtLink
         to="/"
-        class="text-white font-bold px-4 py-1 rounded-lg bg-indigo-600 hover:bg-indigo-700"
+        class="text-white font-bold hover:text-indigo-500 "
         >Contacto</NuxtLink
         >
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,58 @@
 <template>
   <div>
-
-  
-    <div
-      class=" bg-black text-white flex justify-center items-center    h-screen"
-    >
-      <div
-        class="fixed inset-0 bg-gradient-to-br from-[#01111d] via-indigo-700 to-black opacity-40 z-0"
-      ></div>
-      <div class="absolute inset-0 bg-[url('/img/geometry.png')]  opacity-40 z-0"></div>
-      <div
-        class="z-10 text-center  max-w-3xl mx-auto font-cal mt-5"
-      >
-        <h1 class="text-6xl mb-4 font-bold">
+    <div class="bg-gradient-to-br from-[#01111d] via-indigo-700 to-black  text-white flex  h-screen">
+      <div class="bg-black/60 absolute inset-0"></div>
+      <div class="absolute inset-0 bg-[url('/img/geometry.png')] opacity-30 z-0"></div>
+      <div class="z-10 text-center max-w-3xl mx-auto font-cal mt-5">
+        <h2 class="text-white font-black text-5xl font-paris mt-10">
+          Tailors<span class="text-indigo-600">Dev</span>
+        </h2>
+        <h1 class="text-6xl mb-4 font-bold mt-20">
           Construimos Software <br />
-          <span class="text-indigo-600"> a tu Medida.</span>
+          a tu Medida.
         </h1>
-       
+
         <p class="text-2xl mb-4">
-          En <span class="font-black text-indigo-600">TailorsDev</span> diseñamos
-          y desarrollamos <br />
+          En
+          <span class="font-black text-indigo-600">TailorsDev</span> diseñamos y
+          desarrollamos <br />
           soluciones digitales para tu negocio.
         </p>
-      </div>
-      <IconsArrow class="absolute bottom-2 animate-bounce w-6" />
 
-      <div class="absolute right-6 space-y-3 flex flex-col ">
-        <a href="https://wa.me/" class="hover:fill-indigo-500 fill-white" target="_blank">
-          <IconsWhatsapp class="w-8 " />
+        <div class="flex gap-4 justify-center pt-2">
+
+          <button class="bg-indigo-800 hover:scale-105 duration-300 ease-out px-4 text-xl font-bold py-3 rounded-xl">
+            ¡ Contáctanos !
+          </button>
+        </div>
+      </div>
+
+      <div class="absolute right-8 gap-3 flex  bottom-6 z-20">
+        <a href="https://wa.me/" class="hover:scale-105 duration-200 ease-in fill-indigo-900" target="_blank">
+          <IconsWhatsapp class="w-8" />
         </a>
-        <a href="https://www.facebook.com/" class="hover:fill-indigo-500 fill-white" target="_blank">
+        <a href="https://www.facebook.com/" class="hover:scale-105 duration-200 ease-in fill-indigo-900"
+          target="_blank">
           <IconsFacebook class="w-8" />
         </a>
-        <a href="https://www.instagram.com/" class="hover:fill-indigo-500 fill-white" target="_blank">
+        <a href="https://www.instagram.com/" class="hover:scale-105 duration-200 ease-in fill-indigo-900"
+          target="_blank">
           <IconsInstagram class="w-8" />
         </a>
-        <a href="https://www.linkedin.com/" class="hover:fill-indigo-500 fill-white" target="_blank">
-
+        <a href="https://www.linkedin.com/" class="hover:scale-105 duration-200 ease-in fill-indigo-900"
+          target="_blank">
           <IconsLinkedin class="w-8" />
         </a>
       </div>
-      
+
+      <svg xmlns="http://www.w3.org/2000/svg" class="absolute -bottom-20 " viewBox="0 0 1440 320">
+        <path fill="white" fill-opacity="1"
+          d="M0,128L120,149.3C240,171,480,213,720,197.3C960,181,1200,107,1320,69.3L1440,32L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z">
+        </path>
+      </svg>
     </div>
-    
+    <div class="bg-white h-screen">
+      <Proyectos />
     </div>
-  
+  </div>
 </template>
