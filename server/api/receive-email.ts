fbac0a828af8738987746eae4308d313d88e5a2e
David Román <roman.david@gmail.com>
2025-11-11 21:36:47 -0500

    Update receive-email.ts

--- server/api/receive-email.ts
+++ server/api/receive-email.ts
@@ -11,26 +11,12 @@ export default defineEventHandler(async (event) => {
       .receiving
       .get(body.data.email_id);
 
-    const { data: attachments } = await resend
-      .attachments
-      .receiving
-      .list({ emailId: body.data.email_id });
-
-    // Descargar los adjuntos y codificarlos en base64
-    for (const attachment of attachments.data) {
-      const response = await fetch(attachment.download_url);
-      const arrayBuffer = await response.arrayBuffer();
-      const buffer = Buffer.from(arrayBuffer);
-      attachment.content = buffer.toString('base64');
-    }
-
     const { data, error } = await resend.emails.send({
       from: 'Acme <onboarding@resend.dev>',
       to: ['delivered@resend.dev'],
       subject: body.data.subject,
       html: email.html,
       text: email.text,
-      attachments
     });
 
     console.log(error)
